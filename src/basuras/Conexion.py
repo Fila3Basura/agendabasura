95b8e4684d1d6dee7e51f42a90d7d880d89fe072
matiusgg <newpowerexperience2016@gmail.com>
2020-02-19 12:23:11 +0100

    Se ha agregado el contenido del método guardar() de la clase Conexión()

--- src/basuras/Conexion.py
+++ src/basuras/Conexion.py
@@ -26,7 +26,40 @@ class Conexion():
 
 
     def guardar(self, arrayObjetos):
-        pass
+                db = self._conectar()
+        collection = db['diasbasura']
+
+        #* lista que contendrá el resultado de la query de mongoDB
+        listaContenedor = []
+
+        for i in arrayObjetos:
+
+            #* Query de mongoDB para buscar si el documento existe para despues comprobarlo mediante el condicional.
+            buscarDocumento = collection.find(i)
+            #* Si no funciona el anterior query, probar con este.
+            # buscarDocumento = collection.find({'dia': i['dia'], 'basura': i['basura']})
+
+            for i in list(buscarDocumento):
+                #* Se agrega a la lista, porque no funciono colocar list(buscarDocumento) directamente en el condicional.
+                listaContenedor.append(i)
+                
+
+            if listaContenedor != []:
+
+                #* mensaje de que ya existen documentos de algún día
+                mensaje = f'Ya existen el documento del día: {i['dia']}'
+
+                return mensaje
+
+            else:
+
+                #* Método que nos agrega el documento del día a la BD de mongoDB
+                agregarDocumento = collection.insert_one(i)
+
+                #* Mensaje de que aún no exsite un documento de ese día, pero ya se acaba de agregar
+                mensaje = f'No existe el documento del día: {i['dia']}, pero ya se acaba de agregar'
+
+                return mensaje
 
 
 
